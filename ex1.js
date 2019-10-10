3f5f7edf77b3efd3038e41954fdfa33eb116ac6c
Thien Web <thientran1111999@gmail.com>
2019-10-10 09:33:37 +0700

    vidu buoi 3

---
+++ ex1.js
@@ -0,0 +1,12 @@
+// let  -const
+// tu khoa khai bao bien
+
+let s;
+s = "toi la teo";
+console.log(s);
+s = "toi la beo";
+console.log(s);
+const sv ="hoang van be";
+console.log(sv);
+
+sv = "nguyen van ti";
